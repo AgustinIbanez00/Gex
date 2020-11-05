b13460440d1a55044534375b385e64387b221127
Agus12344 <48695094+AgustinIbanez00@users.noreply.github.com>
2020-11-04 23:21:15 -0300

    #6

---
+++ database/migrations/2020_11_03_035843_create_examens_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateExamensTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('examens', function (Blueprint $table) {
+            $table->id();
+            $table->foreign("curso_id")->references("id")->on("cursos");
+            $table->foreign("materia_id")->references("id")->on("materias");
+            $table->string("nombre");
+            $table->dateTime("fecha_creacion");
+            $table->boolean("estado");
+            $table->integer("min_tardanza");
+            $table->integer("nota_regular");
+            $table->integer("nota_promo");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('examens');
+    }
+}
