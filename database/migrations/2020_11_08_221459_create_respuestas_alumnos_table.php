e0d128cfd20896a9ad450cf2a0706e05688f500e
David Ponce <apotheosus@outlook.com>
2020-11-08 19:22:41 -0300

    #10 Respuestas - Alumnos

---
+++ database/migrations/2020_11_08_221459_create_respuestas_alumnos_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRespuestasAlumnosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('respuestas_alumnos', function (Blueprint $table) {
+					$table->id();
+					$table->unsignedBigInteger('alumno_id')->nullable();
+					$table->foreign("alumno_id")->references("id")->on("users");
+					$table->unsignedBigInteger('respuesta_id')->nullable();
+					$table->foreign("respuesta_id")->references("id")->on("respuestas");
+		
+					$table->string("valor")->nullable();
+					$table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('respuestas_alumnos');
+    }
+}
