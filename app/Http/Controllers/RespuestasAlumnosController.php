930c291d95465907fd03ef4c77ee1c8ebc88bfd0
Agus12344 <48695094+AgustinIbanez00@users.noreply.github.com>
2020-11-08 23:08:06 -0300

    #12 Todos los controladores.

---
+++ app/Http/Controllers/RespuestasAlumnosController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\RespuestasAlumnos;
+use Illuminate\Http\Request;
+
+class RespuestasAlumnosController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\RespuestasAlumnos  $respuestasAlumnos
+     * @return \Illuminate\Http\Response
+     */
+    public function show(RespuestasAlumnos $respuestasAlumnos)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\RespuestasAlumnos  $respuestasAlumnos
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(RespuestasAlumnos $respuestasAlumnos)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\RespuestasAlumnos  $respuestasAlumnos
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, RespuestasAlumnos $respuestasAlumnos)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\RespuestasAlumnos  $respuestasAlumnos
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(RespuestasAlumnos $respuestasAlumnos)
+    {
+        //
+    }
+}
--- app/Models/Respuesta.php
+++ app/Models/Respuesta.php
@@ -5,9 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Mesa extends Model
+class Respuesta extends Model
 {
-		use HasFactory;
-		protected $table = 'mesas';
+    use HasFactory;
+    protected $table = 'respuestas';
 }
-
