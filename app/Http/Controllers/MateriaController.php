f0751cba072532a1338902b7524a6387159aef6f
Agus12344 <48695094+AgustinIbanez00@users.noreply.github.com>
2020-11-28 23:18:31 -0300

    Get All Materias.

--- app/Http/Controllers/MateriaController.php
+++ app/Http/Controllers/MateriaController.php
@@ -14,7 +14,9 @@ class MateriaController extends Controller
      */
     public function index()
     {
-    	return view("materias.materias");
+        $materias = Materia::all();
+
+    	return view("materias.materias",  $materias);
     }
 
     /**
